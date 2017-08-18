0598a8fedaeaf15d1ffae7e51bac328b7cd1b0af
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-08-17 23:24:06 -0300

    Update Q6.c

--- Q6.c
+++ Q6.c
@@ -1,34 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-float Percentual()
-{
-
-}
-
 int main()
 {
-  int numjogador[23];
-  int cont = 0;
 
-  while (0 < numjogador <= 23){
-    printf("Enquete: Quem foi o melhor jogador?");
-    printf("\nNumero do jogador: ");
-    scanf("%d", &numjogador);
-
-    if (numjogador > 23){
-      printf("\n Somente votos entre 1 e 23 sao validos");
-    }
-
-    if(numjogador == 0){
-      printf("\nVotacao encerrada");
-      cont++;
-      printf("\nTotal de votos: %d", cont);
-    }
-
-
-  }
-/*char jogadores[10];
+    char jogadores[10];
     int voto = 1;
     int i, j;
 
@@ -52,7 +28,7 @@ int main()
             {
                 printf("Votacao encerrada");
                 break;
-            }*/
+            }
   return 0;
 
 }
