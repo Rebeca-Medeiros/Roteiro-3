28a9ce84fcd5e3ab35447897c787d3321039d912
Lília <liliarc96@gmail.com>
2017-08-17 23:20:34 -0300

    Add files via upload
    No comentário

--- Q6.c
+++ Q6.c
@@ -28,7 +28,31 @@ int main()
 
 
   }
+/*char jogadores[10];
+    int voto = 1;
+    int i, j;
 
+    printf("Enquete: Quem foi o melhor jogador?");
+    scanf("%d", &voto);
+
+        for(j = 0; j <= 10; j++)
+        {
+            if (voto > 0 && voto <= 23)
+            {
+                scanf("%d", &voto);
+                jogadores[j] = voto;
+                i++; //contagem de votos
+            }
+            if (voto > 23)
+            {
+                printf("Voto invalido. Digite novamente");
+                scanf("%d", &voto);
+            }
+            if (voto == 0)
+            {
+                printf("Votacao encerrada");
+                break;
+            }*/
   return 0;
 
 }
